6477b6957a25cd4a2501828824eee216a2d2800f
jue wang <joycewang@jues-MacBook-Pro.local>
2021-05-28 21:44:08 -0700

    convert str in ca_county_tiers_imputed to timestamp

--- preprocessing.py
+++ preprocessing.py
@@ -72,8 +72,15 @@ def find_tier(tier_df,county,time):
         return 'Unknown'
     if time=='Unknown':
         return 'Unknown'
+    if type(time) is str:
+            print(time)
+            return 'Unknown'
     county_history = tier_df.loc[county,:]
     for start,end in zip(county_history.index[:-1],county_history.index[1:] ):
+        if type(start) is str:
+                print(start)
+                continue
+        
         if start <= time < end:
             return str(county_history[start])
     return 'Unknown'
@@ -95,7 +102,7 @@ def add_count_symptoms(df,symptom_list):
 
 
 #file_path = './COVID-19_Case_Surveillance_Public_Use_Data_with_Geography.csv'
-file_path = './covid_case_restricted_detailed-master/data/CA_combined_csv.csv'
+file_path = 'CA_combined_csv.csv'
 df_raw = pd.read_csv(file_path,sep=',',error_bad_lines=False, engine="python")
 df_raw = df_raw.replace({'MISSING':'Unknown','Missing':'Unknown'})
 df_raw['time'] =pd.to_datetime(df_raw['cdc_case_earliest_dt'],format= '%Y-%m-%d')
@@ -107,11 +114,13 @@ df_raw["age_raceethnicity_combined"] = df_raw["race_ethnicity_combined"] + df_ra
 
 
 #add features
-df_add = add_feature(df,'income_race.csv',['race_ethnicity_combined','res_county'])
+df_add = add_feature(df_raw,'income_race_imputed.csv',['race_ethnicity_combined','res_county'])
 df_add = add_feature(df_add,'demographics_df_imputed.csv','res_county')
 df_add = add_feature(df_add,'geographic_region.csv','res_county')
 df_add['time'] =pd.to_datetime(df_add['cdc_case_earliest_dt'],format= '%Y-%m-%d')
 tier_df = pd.read_csv('ca_county_tiers_imputed.csv',index_col=0)
+tier_df.columns = pd.to_datetime(tier_df.columns,format= '%Y-%m-%d')
+
 df_add['tier'] = [find_tier(tier_df,county,time) for (county,time) in zip(df_add['res_county'],df_add['time']) ]
 df_add = add_feature(df_add,'vaccine_time_county_imputed.csv',['res_county','cdc_case_earliest_dt'])
 
@@ -131,10 +140,12 @@ symptom_list = ['fever_yn', 'sfever_yn',
        'sob_yn', 'nauseavomit_yn', 'headache_yn', 'abdom_yn', 'diarrhea_yn',
        'medcond_yn']
 df_add = add_count_symptoms(df_add,symptom_list)
+df_add.to_csv(r'./CA_cleaned.csv',index = False)
+
 #onehot_cols = ['case_month','res_state','age_group','sex','race','ethnicity','process','current_status','symptom_status']
 #binary_list = ['hosp_yn','icu_yn','underlying_conditions_yn','exposure_yn']
 #maxmin_cols = ['case_positive_specimen_interval','case_onset_interval']
 #df_encoded = pd.concat([class_encode(df_raw[['death_yn']]),scaling(df_raw[maxmin_cols]),binary_encode(df_raw[binary_list]),onehot_encode(df_raw[onehot_cols])],axis=1)
-df_encoded = pd.concat([onehot_encode(df_add[one_hot_list]),to_month_encode(df_add[time_list]),df_add[numerical_list]],axis = 1)
+#df_encoded = pd.concat([onehot_encode(df_add[one_hot_list]),to_month_encode(df_add[time_list]),df_add[numerical_list]],axis = 1)
 #df_encoded = df_encoded[df_encoded['death_yn']!=0]
-df_encoded.to_csv(r'./CA_cleaned.csv',index = False)
+#df_encoded.to_csv(r'./CA_encoded.csv',index = False)
